ce33e7b68d14bfed137db9306ab42800f46663ef
kasalpe2 <kasalpe2@fit.cvut.cz>
2023-10-28 18:24:27 +0200

    topsort with longestPath

--- RaceTrack/RaceTrack.cpp
+++ RaceTrack/RaceTrack.cpp
@@ -43,37 +43,98 @@ struct Path
 #endif
 using namespace std;
 
+vector<Path> TopologicalSort(const std::unordered_map<size_t, std::vector<Path>>& graph) {
+    std::unordered_map<size_t, int> in_degree;
+    std::queue<size_t> zero_in_degree;
 
+    for (const auto& entry : graph) {
+        for (const Path& edge : entry.second) {
+            in_degree[edge.to]++;
+        }
+    }
 
+    for (const auto& entry : graph) {
+        size_t node = entry.first;
+        if (in_degree.find(node) == in_degree.end() || in_degree[node] == 0) {
+            zero_in_degree.push(node);
+        }
+    }
 
-std::vector<Path> longest_track(size_t points, const std::vector<Path> &all_paths)
+    // Inicializace délky nejdelší cesty do každého vrcholu.
+    std::unordered_map<size_t, unsigned> longest_path;
+    for (const auto& entry : graph) {
+        longest_path[entry.first] = 0;
+    }
+
+    // Inicializace předchůdců pro rekonstrukci nejdelší cesty.
+    std::unordered_map<size_t, size_t> predecessors;
+
+    while (!zero_in_degree.empty()) {
+        size_t node = zero_in_degree.front();
+        zero_in_degree.pop();
+
+        for (const Path& edge : graph.at(node)) {
+            size_t neighbor = edge.to;
+            if (longest_path[node] + edge.length > longest_path[neighbor]) {
+                longest_path[neighbor] = longest_path[node] + edge.length;
+                predecessors[neighbor] = node;
+            }
+
+            in_degree[neighbor]--;
+            if (in_degree[neighbor] == 0) {
+                zero_in_degree.push(neighbor);
+            }
+        }
+    }
+
+    // Rekonstrukce nejdelší cesty od posledního vrcholu zpět k prvnímu.
+    size_t current_node = 0;  // První vrchol
+    unsigned max_length = 0;
+    for (const auto& entry : longest_path) {
+        if (entry.second > max_length) {
+            max_length = entry.second;
+            current_node = entry.first;
+        }
+    }
+
+    std::vector<Path> longest_path_edges;
+    while (predecessors.find(current_node) != predecessors.end()) {
+        size_t predecessor = predecessors[current_node];
+        for (const Path& edge : graph.at(predecessor)) {
+            if (edge.to == current_node) {
+                longest_path_edges.push_back(edge);
+                break;
+            }
+        }
+        current_node = predecessor;
+    }
+    
+   std::reverse(longest_path_edges.begin(), longest_path_edges.end());
+    // Výpis nejdelší cesty a její délky.
+    // std::cout << "Nejdelší cesta: ";
+    // for (const Path& edge : longest_path_edges) {
+    //     std::cout << "{" << edge.from << ", " << edge.to << ", " << edge.length << "} ";
+    // }
+    // std::cout << "Celková délka: " << max_length << std::endl;
+    return longest_path_edges;
+}
+
+vector<Path> longest_track(size_t points, const vector<Path> &all_paths)
 {
-  std::unordered_map<size_t, std::vector<Path>> graph;
+  unordered_map<size_t, vector<Path>> graph;
   for (const Path &path : all_paths)
   {
+    // Přidejte hranu ze "from" do "to" do seznamu hran v mapě "graph".
     graph[path.from].push_back(path);
-  }
-  cout << "----------------------" << endl;
-  for (const auto &entry : graph)
-  {
-    size_t krizovatka = entry.first;
-    const std::vector<Path> &cesty = entry.second;
-
-    std::cout << "Křižovatka " << krizovatka << ":" << std::endl;
-
-    for (const Path &cesta : cesty)
+    // Pokud vrchol "to" není v mapě "graph", přidejte ho s prázdným seznamem hran.
+    if (graph.find(path.to) == graph.end())
     {
-      std::cout << "  Cesta z " << cesta.from << " do " << cesta.to << " s délkou " << cesta.length << std::endl;
+      graph[path.to] = vector<Path>();
     }
   }
-  cout << "----------------------" << endl;
 
   vector<Path> result;
-
-  for (const Path &path : result)
-  {
-    std::cout << "From " << path.from << " to " << path.to << ", Length: " << path.length << std::endl;
-  }
+  result = TopologicalSort(graph);
   return result;
 }
 
@@ -83,7 +144,7 @@ struct Test
 {
   unsigned longest_track;
   size_t points;
-  std::vector<Path> all_paths;
+  vector<Path> all_paths;
 };
 
 inline const Test TESTS[] = {
@@ -115,7 +176,7 @@ bool run_test(const Test &t)
 
   for (size_t i = 0; i < sol.size(); i++)
   {
-    CHECK(std::count(t.all_paths.begin(), t.all_paths.end(), sol[i]),
+    CHECK(count(t.all_paths.begin(), t.all_paths.end(), sol[i]),
           "Solution contains non-existent path: %zu -> %zu (%u)",
           sol[i].from, sol[i].to, sol[i].length);
 
